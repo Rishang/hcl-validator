2ff58bbcfa0230789935f7307c4647fa17cd3eed
rishang@openxcellinc.com <rishang@openxcellinc.com>
2022-03-11 17:00:27 +0530

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+const actions = require('@actions/core')
+const fs = require('fs')
+const hcl = require("js-hcl-parser")
+
+let hcl_data
+const file_path = actions.getInput('HCL_FILE_PATH') ||  process.env.HCL_FILE_PATH
+
+try {
+    hcl_data = fs.readFileSync(file_path, 'utf8')
+} catch (err) {
+    console.error(err)
+}
+
+let hcl_string = hcl.parse(hcl_data)
+
+
+if (hcl_string.includes("unable to parse JSON")) {
+    throw new Error(hcl_string)
+}
+else {
+    console.log(`SUCESS: ${file_path} is valid hcl file.`)
+}
+
