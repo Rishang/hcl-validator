8c76c161432dabf798c61b2ded297b1940ed64ce
oxrishang <rishang@openxcellinc.com>
2022-03-15 10:26:12 +0530

    multi paths

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # hcl syntax validator for github actions
 
+### Required arguments
 **Usage**
+
+ - **HCL_FILE_PATH**: provide list of folder of file paths seprated by "," to check syntac of.
+
+### Example actions
 
 ```yml
 - uses: oxrishang/hcl-validator@v1
     with:
-    HCL_FILE_PATH: ./vars/test.tfvars
+    HCL_FILE_PATH: ./vars, ./test/demo.hcl, ./deploy/conf.tfvars
 ```
--- main.js
+++ main.js
@@ -3,22 +3,63 @@ const fs = require('fs')
 const hcl = require("js-hcl-parser")
 
 let hcl_data
-const file_path = actions.getInput('HCL_FILE_PATH') ||  process.env.HCL_FILE_PATH
+let file_paths = actions.getInput('HCL_FILE_PATH') ||  process.env.HCL_FILE_PATH
 
-try {
-    hcl_data = fs.readFileSync(file_path, 'utf8')
-} catch (err) {
-    console.error(err)
+// remove spaces and get list of paths seprated by ","
+file_paths = file_paths.replace(' ','').split(',')
+
+function check_ext(file_path) {
+    let valid = ["hcl","tf","tfvars"]
+  
+    let f_ext = file_path.split('/').at(-1).split('.').at(-1)
+  
+    if (valid.includes(f_ext)) {
+      return true
+    }
+    else {
+      return false
+    }
 }
 
-let hcl_string = hcl.parse(hcl_data)
 
+function check_hcl(file_path) {
 
-if (hcl_string.includes("unable to parse JSON")) {
-    let message = `FILE: ${file_path} | ${hcl_string}`
-    throw new Error(message)
+    try {
+        hcl_data = fs.readFileSync(file_path, 'utf8')
+    } catch (err) {
+        console.error(err)
+    }
+    
+    let hcl_string = hcl.parse(hcl_data)
+    
+    
+    if (hcl_string.includes("unable to parse JSON")) {
+        let message = `FILE: ${file_path} | ${hcl_string}`
+        throw new Error(message)
+    }
+    else {
+        console.log(`SUCESS: ${file_path} is valid hcl file.`)
+    }
 }
-else {
-    console.log(`SUCESS: ${file_path} is valid hcl file.`)
-}
 
+
+
+// console.log(file_paths)
+
+for (let path of file_paths) {
+
+    if (fs.statSync(path).isDirectory()) {
+        _sub_fs = fs.readdirSync(path)
+
+        for (let _f of _sub_fs) {
+            let f = `${path}/${_f}`
+
+            if (fs.statSync(f).isFile() && check_ext(f)) {
+                check_hcl(f)
+            }
+        }
+    }
+    else if (fs.statSync(path).isFile() && check_ext(path)) {
+        check_hcl(path)
+    }
+}
